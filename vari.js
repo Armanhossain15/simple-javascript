a62282273f593b3bab5fc93904c643264930be4d
Armanhossain15 <armanhossain3574@gmail.com>
2024-01-26 16:51:31 +0600

    first commit

---
+++ vari.js
@@ -0,0 +1,78 @@
+// var nam =parse "arman";
+// var clg = parseInt "MBPI";
+// var semester = parseInt("7");
+// var roll = 579410;
+// var isrich = true;
+// console.log(typeof isrich)
+// // console.log(typeof roll);
+// // console.log(typeof isrich);
+// // console.log(nam);
+// // console.log(clg);
+// // console.log(semester);
+// console.log(semester+roll);
+
+// var first= 0.1;
+// var second = 0.2;
+// var total = first+second;
+// console.log(parseInt(total. toFixed(2)));
+
+
+// var num = 455.666654434;
+// console.log(num.toFixed(3));
+
+// var price = 344.543532;
+// console.log(price.toFixed(2));
+
+
+// var num =isNaN(5);
+// console.log(num);
+
+// var price = isNaN(5-"Two");
+// console.log(price);
+
+
+/////////////////////////// practice task 1
+
+// var budget = 1000;
+// var orange = 100;
+// var orangeBuy = orange * 2;
+// var apples = 80; 
+// var applesBuy = apples * 4;
+
+// var toBuy = orangeBuy + applesBuy;
+// console.log(orangeBuy);
+// console.log(applesBuy);
+// console.log(toBuy);
+// console.log(budget-toBuy);
+
+
+//////////////////////practice 2
+
+// var math = 78.43;
+// var english = 955.53;
+// var bangla = 8.549;
+// var physics = 60;
+// var biology = 67;
+// var totalMark = math+ english+ bangla+ physics+ biology;
+// var gor = parseInt ( totalMark / 5);
+// var gor2 = parseInt(gor);
+// console.log(totalMark);
+// // var output = console.log(typeof gor.toFixed(2));
+// console.log(gor2);
+
+
+//////////////////  practice 3
+
+// var num = 5;
+// var result = num%2;
+// console.log(result);
+
+///////// practice 4
+
+var a = isNaN('3232323');
+console.log( a);
+
+
+var b = isNaN ('2-1');
+console.log(typeof b);
+
